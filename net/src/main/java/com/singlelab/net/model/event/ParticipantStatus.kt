dafaf0ee07c36569dabb3bc80ed45048303ad986
julia-black <b-r-cat@ya.ru>
2020-08-17 14:12:27 +0400

    Added new participant status

--- net/src/main/java/com/singlelab/net/model/event/ParticipantStatus.kt
+++ net/src/main/java/com/singlelab/net/model/event/ParticipantStatus.kt
@@ -3,7 +3,8 @@ package com.singlelab.net.model.event
 enum class ParticipantStatus(val id: Int) {
     WAITING_FOR_APPROVE_FROM_USER(0),
     WAITING_FOR_APPROVE_FROM_EVENT(1),
-    ACTIVE(2);
+    ACTIVE(2),
+    CLOSED(3);
 
     companion object {
         fun findStatus(id: Int?): ParticipantStatus? {
