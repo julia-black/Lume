9bf2b815c680e638e32a264532e53b52b16f17da
julia-black <b-r-cat@ya.ru>
2020-09-04 11:47:09 +0400

    Added new tutorial pictures

--- app/src/main/java/com/singlelab/lume/model/tutorial/TutorialPage.kt
+++ app/src/main/java/com/singlelab/lume/model/tutorial/TutorialPage.kt
@@ -3,9 +3,9 @@ package com.singlelab.lume.model.tutorial
 import com.singlelab.lume.R
 
 enum class TutorialPage(val imageId: Int, val titleId: Int) {
-    TUTORIAL_1(R.drawable.ic_tutorial_1, R.string.tutorial_text_1),
-    TUTORIAL_2(R.drawable.ic_tutorial_2, R.string.tutorial_text_2),
-    TUTORIAL_3(R.drawable.ic_tutorial_3, R.string.tutorial_text_3),
+    TUTORIAL_1(R.drawable.ic_1, R.string.tutorial_text_1),
+    TUTORIAL_2(R.drawable.ic_2, R.string.tutorial_text_2),
+    TUTORIAL_3(R.drawable.ic_3, R.string.tutorial_text_3),
     TUTORIAL_4(R.drawable.ic_tutorial_4, R.string.tutorial_text_4),
     TUTORIAL_5(R.drawable.ic_tutorial_5, R.string.tutorial_text_5)
 }
