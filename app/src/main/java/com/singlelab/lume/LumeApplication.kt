f6aaf9c60a687a32b647e3af9b93f866b439653c
julia-black <b-r-cat@ya.ru>
2020-10-16 15:43:37 +0400

    Added Appmetrica analytics

--- app/src/main/java/com/singlelab/lume/LumeApplication.kt
+++ app/src/main/java/com/singlelab/lume/LumeApplication.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import android.content.Context
 import com.singlelab.lume.model.Const
 import com.singlelab.lume.pref.Preferences
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class LumeApplication : Application() {
@@ -15,5 +18,12 @@ class LumeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         preferences = Preferences(getSharedPreferences(Const.PREF, Context.MODE_PRIVATE))
+        initAppmetrica()
+    }
+
+    private fun initAppmetrica() {
+        val config = YandexMetricaConfig.newConfigBuilder(getString(R.string.appmetrica_key)).build()
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
     }
 }
